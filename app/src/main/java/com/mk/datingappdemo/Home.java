e7e94f4921aacee94977676b4db7f4e1123bcc0a
Manikaran <kathuriamanikaran.com>
2018-10-09 16:26:59 +0530

    GUI updated

--- app/src/main/java/com/mk/datingappdemo/Home.java
+++ app/src/main/java/com/mk/datingappdemo/Home.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -19,15 +24,23 @@ public class Home extends AppCompatActivity implements OnItemClickListener {
 
     private final String[] options = {"Messages", "Will Respond", "Search", "Nearby", "My Matches",
     "Viewed Me", "Meet Me", "Profile", "Favorites"};
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.home2);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
 
         RecyclerView optionRecyclerView = findViewById(R.id.optionGridRecyclerView);
-        optionRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
-        optionRecyclerView.addItemDecoration(new SpacesItemDecoration(25,25, 50));
+        //optionRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        optionRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        optionRecyclerView.addItemDecoration(new SpacesItemDecoration(25,25, 25));
         optionRecyclerView.setAdapter(new OptionRecyclerViewAdapter(this));
 
         RecyclerView prospectsRecyclerView = findViewById(R.id.prospectsRecyclerView);
@@ -35,13 +48,40 @@ public class Home extends AppCompatActivity implements OnItemClickListener {
         prospectsRecyclerView.addItemDecoration(new SpacesItemDecoration(20,20,10));
         prospectsRecyclerView.setAdapter(new ProspectsRecyclerViewAdapter());
 
+        drawerLayout = findViewById(R.id.mDrawerLayout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()) {
+                    case R.id.profile: {
+                        Intent intent = new Intent(Home.this, Profile.class);
+                        startActivity(intent);
+                    }
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onItemClick(String name) {
         switch (name) {
             case "Profile": {
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(this, Profile.class);
                 startActivity(intent);
             }
             break;
@@ -62,7 +102,8 @@ public class Home extends AppCompatActivity implements OnItemClickListener {
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             //if(viewType == 0) {
-            View view = LayoutInflater.from(Home.this).inflate(R.layout.home_recycler_view_option_element, parent, false);
+            //View view = LayoutInflater.from(Home.this).inflate(R.layout.home_recycler_view_option_element, parent, false);
+            View view = LayoutInflater.from(Home.this).inflate(R.layout.home_recycler_view_images_element, parent, false);
             return new ViewHolder(view);
             //}
 
@@ -71,12 +112,12 @@ public class Home extends AppCompatActivity implements OnItemClickListener {
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
             ViewHolder viewHolder = (ViewHolder) holder;
-            viewHolder.onBind(options[position], R.mipmap.ic_launcher, listener);
+            viewHolder.onBind(options[position], R.drawable.boy, listener);
         }
 
         @Override
         public int getItemCount() {
-            return 9;
+            return 6;
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
@@ -84,13 +125,15 @@ public class Home extends AppCompatActivity implements OnItemClickListener {
             TextView tv;
             ViewHolder(View itemView) {
                 super(itemView);
-                iv = itemView.findViewById(R.id.icon);
-                tv = itemView.findViewById(R.id.label);
+                iv = itemView.findViewById(R.id.imageView);
+
+                //iv = itemView.findViewById(R.id.icon);
+                //tv = itemView.findViewById(R.id.label);
             }
 
             void onBind(final String name, int drawable, final OnItemClickListener listener) {
                 iv.setImageResource(drawable);
-                tv.setText(name);
+                //tv.setText(name);
                 itemView.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
--- app/src/main/java/com/mk/datingappdemo/Profile.java
+++ app/src/main/java/com/mk/datingappdemo/Profile.java
@@ -8,25 +8,41 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class Profile extends AppCompatActivity implements View.OnClickListener {
 
     ImageView personImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        setContentView(R.layout.profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.username);
         ListView profileDetails = findViewById(R.id.profile_list);
         Button uploadButton = findViewById(R.id.uploadImage);
         personImageView = findViewById(R.id.personImage);
+        LinearLayout containerView = findViewById(R.id.containerView);
+        String[] labels = getResources().getStringArray(R.array.profile_labels);
+        String[] values = getResources().getStringArray(R.array.profile_values);
+        for(int i=0;i<labels.length;i++) {
+            View view = LayoutInflater.from(this).inflate(R.layout.profile_list_element, null, false);
+            ((TextView)view.findViewById(R.id.label)).setText(labels[i]);
+            ((TextView)view.findViewById(R.id.value)).setText(values[i]);
+            containerView.addView(view);
+
+        }
+
+
         ProfileListAdapter adapter = new ProfileListAdapter(this, R.layout.profile_list_element);
         profileDetails.setAdapter(adapter);
         uploadButton.setOnClickListener(this);
